6fae6a75e7a660856f11728dac2c9f4ca9088b48
Jessie Morris <jmorrisosu@gmail.com>
2024-10-25 01:10:58 -0400

    city background, can rotation

--- Assets/Scrips/CanControls.cs
+++ Assets/Scrips/CanControls.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class CanControls : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Sprite[] canSprites;
+
+    private float speed = 5;
+    private SpriteRenderer spriteRenderer;
+    private int spriteIndex = 0;
+
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (horizontal != 0)
+        {
+            float newX = Mathf.Clamp(transform.position.x + (horizontal * speed * Time.deltaTime), -8f, 8f);
+            transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RotateThroughCans();
+        }
     }
+    void RotateThroughCans()
+    {
+        spriteIndex++;
+        if (spriteIndex >= canSprites.Length)
+        {
+            spriteIndex = 0;
+        }
+        spriteRenderer.sprite = canSprites[spriteIndex];
+    }
+
+    // private Vector3 BoundToScreen(Vector3 newPosition)
+    // {
+    //     float halfWidth = spriteRenderer.sprite.bounds.size.x;
+    //     float halfHeight = spriteRenderer.sprite.bounds.size.y;
+
+    //     newPosition.x = Mathf.Clamp(newPosition.x, screenBounds.x * -1 + halfWidth, screenBounds.x - halfWidth);
+    //     newPosition.y = Mathf.Clamp(newPosition.y, screenBounds.y * -1 + halfHeight, screenBounds.y - halfHeight);
+    //     return newPosition;
+    // }
 }
