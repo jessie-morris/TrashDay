3d3f89f4d99bf78f0545a2cf6d074eb0e5c6cbd2
Jessie Morris <jmorrisosu@gmail.com>
2024-10-26 03:03:18 -0400

    Improved story screen

---
+++ Assets/Scripts/StoryManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoryManager : MonoBehaviour
+{
+    [SerializeField] private TMP_Text storyText;
+    [SerializeField] private Button startGameButton;
+    string storyString =
+    @"After waking up from a coma, you see that the bitcoin price has jumped from a humble eight dollars to over $60,000
+
+
+Your landlord threw out your belongings including the harddrive containing 100 bitcoins!
+
+
+After calling the city, they've allowed you to search for your lost harddrive on the condition you help clean up the city.
+
+
+Clean up the city and dig through pieces of your past in the hope you'll find your fortune!";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ShowStory());
+    }
+
+    IEnumerator ShowStory()
+    {
+        foreach (char c in storyString)
+        {
+            yield return new WaitForSeconds(0.05f);
+            storyText.text += c;
+        }
+
+        startGameButton.gameObject.SetActive(true);
+    }
+}
