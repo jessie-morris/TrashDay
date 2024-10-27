52bb8b302ab6b95607de5491ef881ddf2e38be30
Jacob Rutter <rutter.jacob@gmail.com>
2024-10-27 02:16:45 -0400

    Added .5 pause to allow click sound
    Click sound is more of a "pop" may want to reconsider

--- Assets/Scripts/MenuControls.cs
+++ Assets/Scripts/MenuControls.cs
@@ -6,48 +6,98 @@ public class MenuControls : MonoBehaviour
 {
     public void StartGame_OnClick()
     {
+        StartCoroutine(LoadStoryScene());
+    }
+
+    IEnumerator LoadStoryScene()
+    {
         AudioManager.instance.PlayOneShot("Click");
+        yield return new WaitForSeconds(.5f);
         UnityEngine.SceneManagement.SceneManager.LoadScene("StoryScene");
     }
 
     public void CleanupTime_OnClick()
     {
+        StartCoroutine(LoadInstructionsScene());
+    }
+
+    IEnumerator LoadInstructionsScene()
+    {
         AudioManager.instance.PlayOneShot("Click");
+        yield return new WaitForSeconds(.5f);
         UnityEngine.SceneManagement.SceneManager.LoadScene("InstructionsScene");
     }
 
     public void Ok_OnClick()
     {
+        StartCoroutine(LoadCleanupScene());
+    }
+
+    IEnumerator LoadCleanupScene()
+    {
         AudioManager.instance.PlayOneShot("Click");
+        yield return new WaitForSeconds(.5f);
         UnityEngine.SceneManagement.SceneManager.LoadScene("CleanupScene");
     }
 
     public void Back_OnClick()
     {
+        StartCoroutine(LoadMidgameMenuScene());
+    }
+
+    IEnumerator LoadMidgameMenuScene()
+    {
         AudioManager.instance.PlayOneShot("Click");
+        yield return new WaitForSeconds(.5f);
         UnityEngine.SceneManagement.SceneManager.LoadScene("MidgameMenuScene");
     }
 
     public void SearchDumpsters_OnClick()
     {
+        StartCoroutine(LoadTrashShopScene());
+    }
+
+    IEnumerator LoadTrashShopScene()
+    {
         AudioManager.instance.PlayOneShot("Click");
+        yield return new WaitForSeconds(.5f);
         UnityEngine.SceneManagement.SceneManager.LoadScene("TrashShopScene");
     }
+
     public void Hints_OnClick()
     {
-        AudioManager.instance.PlayOneShot("Click");
-        UnityEngine.SceneManagement.SceneManager.LoadScene("HintsScene");
+        StartCoroutine(LoadHintsScene());
     }
-    public void Computer_OnClick()
+
+    IEnumerator LoadHintsScene()
     {
         AudioManager.instance.PlayOneShot("Click");
+        yield return new WaitForSeconds(.5f);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("HintsScene");
+    }
+
+    public void Computer_OnClick()
+    {
+        StartCoroutine(LoadComputerScene());
+    }
+
+    IEnumerator LoadComputerScene()
+    {
+        AudioManager.instance.PlayOneShot("Click");
+        yield return new WaitForSeconds(.5f);
         UnityEngine.SceneManagement.SceneManager.LoadScene("ComputerScene");
     }
 
     public void EarnCredits_OnClick()
     {
         GameManager.instance.resetTimeElapsed();
+        StartCoroutine(LoadCleanupScene());
+    }
+
+    IEnumerator LoadCleanUpScene()
+    {
         AudioManager.instance.PlayOneShot("Click");
+        yield return new WaitForSeconds(.5f);
         UnityEngine.SceneManagement.SceneManager.LoadScene("CleanupScene");
     }
 
