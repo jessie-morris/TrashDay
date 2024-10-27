4156023259bc82e866dea06f92add8ce7953ebf1
Jacob Rutter <rutter.jacob@gmail.com>
2024-10-27 03:14:48 -0400

    Added Animations for Score Text

--- Assets/Scripts/CreditsManager.cs
+++ Assets/Scripts/CreditsManager.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class CreditsManager : MonoBehaviour
 {
     private int credits = 0;
+    [SerializeField] private Animator newCreditsAnimator;
+    [SerializeField] private Animator totalCreditsAnimator;
 
     public static CreditsManager instance;
 
@@ -25,7 +27,29 @@ public class CreditsManager : MonoBehaviour
     public void AddToScore(int points)
     {
         credits += points;
+        StartCoroutine(AnimateCreditsText());
     }
+
+    IEnumerator AnimateCreditsText()
+    {
+        if (newCreditsAnimator != null)
+        {
+            newCreditsAnimator.SetBool("AddCredits", true); //show score being added
+            yield return new WaitForSeconds(.3f);
+            if (totalCreditsAnimator != null)
+            {
+                totalCreditsAnimator.SetBool("GrowCredits", true); //emphasize score
+                yield return new WaitForSeconds(1);
+                
+                if (newCreditsAnimator != null && totalCreditsAnimator != null)
+                {
+                    totalCreditsAnimator.SetBool("GrowCredits", false); //reset
+                    newCreditsAnimator.SetBool("AddCredits", false); //reset
+                }
+            }
+        }
+    }
+
     public void DeductFromScore(int points)
     {
         credits -= points;
